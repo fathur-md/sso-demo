a0254fb9f057ec29e0645c4345ad1cb253851bae
Fathur Muhammad <fathurmh@98gmail.com>
2025-08-21 16:41:51 +0700

    update public json

--- src/components/dashboard/Content.jsx
+++ src/components/dashboard/Content.jsx
@@ -1,8 +1,10 @@
 export const Content = ({ user }) => {
   return (
     <div className="w-full">
-      <div className="glass-bg w-full rounded-lg p-4">
-        <h1>Welcome</h1>
+      <div>
+        <div className="glass-bg w-full rounded-lg p-4 md:max-w-lg">
+          <h1 className="text-2xl">Welcome</h1>
+        </div>
       </div>
     </div>
   );
